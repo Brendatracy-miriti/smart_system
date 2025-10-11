5214f699f1d2376c64df42bd8f26f9d8cf3f0dd8
Chalie <pwnipc@proton.me>
2025-10-11 13:00:52 +0300

    [feat] add PWD configs

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    injectRegister: 'auto',
+    registerType: 'autoUpdate',
+    devOptions: { enabled: true },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico,json}']
+    }
+  })],
 })
