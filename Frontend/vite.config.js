c6e3471f9a55facd8a8e91cd99bc2c38c368552a
Chalie <pwnipc@proton.me>
2025-10-11 14:03:58 +0300

    [refactor] update pwa configs

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,11 +5,44 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    injectRegister: 'auto',
     registerType: 'autoUpdate',
     devOptions: { enabled: true },
     workbox: {
       globPatterns: ['**/*.{js,css,html,png,svg,ico,json}']
-    }
+    },
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+        name: 'Edu-Guardian',
+        short_name: 'Edu-Guardian',
+        description: 'Safe. Transparent. Smart. Connect your school community in one platform.',
+        theme_color: '#ffffff',
+        display: 'fullscreen',
+        start_url: '/index.html',
+        background_color: '#ffffff',
+        icons: [
+            {
+                src: 'pwa-64x64.png',
+                sizes: '64x64',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any'
+            },
+            {
+                src: 'maskable-icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable'
+            }
+        ],
+      }
   })],
 })
